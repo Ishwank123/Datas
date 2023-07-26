3f822b8729a17c08940f7b38d52ba5abaa30c0b2
Ishwank123 <140597738+Ishwank123@users.noreply.github.com>
2023-07-26 14:38:06 +0530

    Add files via upload

---
+++ vds1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cstring>
+#include <string>
+using namespace std;
+ int main()
+ {
+     string name;
+     getline(cin,name);
+     cout<<"The initial string is:";
+     cout<<name;
+     
+ }
+ 
